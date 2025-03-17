cddc6dce9a92822ae5729982c1a1fc2c2bc0146e
Anatoliy <119749324+Sskame1@users.noreply.github.com>
2025-03-17 23:51:26 +0900

    changed create-user dto(added validation)

--- backend/src/auth/create-user.dto.ts
+++ backend/src/auth/create-user.dto.ts
@@ -1,5 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator"
+
 export class CreateUserDto {
-    name: string
-    email: string
-    password: string
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(20)
+    name: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(6)
+    @MaxLength(20)
+    password: string;
 }
