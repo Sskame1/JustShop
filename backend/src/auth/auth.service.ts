cbc778eb37ddef734bd209bb7daf0369c34828e3
Anatoliy <119749324+Sskame1@users.noreply.github.com>
2025-03-17 23:30:05 +0900

    added in auth service register

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private prisma: PrismaService) {}
+
+    async register(CreateUserDto: CreateUserDto) {
+        const { name, email, password } = CreateUserDto;
+        const user = await this.prisma.user.create({
+            data: {
+                name,
+                email,
+                password, //no hashing
+            },
+        });
+        return user;
+    }
+}
