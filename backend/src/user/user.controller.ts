fc5e1bd1f3f3f0d8bec41c5bd0fab9d5696a13a0
Anatoliy <119749324+Sskame1@users.noreply.github.com>
2025-03-18 23:36:13 +0900

    changed user service and controller(added get one user and all user's)

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe)id: number) {
+    return this.userService.findOne(id);
+  }
 }
