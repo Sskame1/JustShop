88f45aec5ca4c2f6c55df8e32cc0b70016454a54
Anatoliy <119749324+Sskame1@users.noreply.github.com>
2025-03-19 00:41:23 +0900

    changed auth controller and service(added login endpoint and JWT implementation)

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './create-user.dto';
+import { LoginUserDto } from './login-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
   @Post('register')
   async register(@Body() createUserDto: CreateUserDto) {
     return this.authService.register(createUserDto);
+  }
+
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto.email, loginUserDto.password);
   }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './create-user.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) {}
+    constructor(
+        private prisma: PrismaService,
+        private JwtService: JwtService,
+    ) {}
 
     async register(CreateUserDto: CreateUserDto) {
         const { name, email, password } = CreateUserDto;
@@ -22,5 +26,25 @@ export class AuthService {
 
         const { password: _, ...result } = user
         return result;
+    }
+
+    async login(email: string, password: string) {
+        const user = await this.prisma.user.findUnique({
+            where: { email },
+        });
+
+        if (!user) {
+            throw new  UnauthorizedException('Invalid email or password');
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            throw new UnauthorizedException('Invalid email or password');
+        }
+
+        const payload = { email: user.email, sub: user.id };
+        return {
+            access_token: this.JwtService.sign(payload),
+        };
     }
 }
