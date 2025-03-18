f5d0d3479c35ae3e9710f796e1a62bc643a70b11
Anatoliy <119749324+Sskame1@users.noreply.github.com>
2025-03-19 00:25:05 +0900

    changed auth module(added config for JWT)

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,8 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, PrismaService],
 })
