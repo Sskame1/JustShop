f6fd8b713458d5b6e46598693aab575dfdee490e
Anatoliy <119749324+Sskame1@users.noreply.github.com>
2025-03-19 21:12:08 +0900

    added in produc service func update and remove product

--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -1,10 +1,11 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProductDto } from './create-product.dto';
+import { UpdateProductDto } from './update-product.dto';
 
 @Injectable()
 export class ProductService {
-    constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) { }
 
     async create(createProductDto: CreateProductDto) {
         return this.prisma.product.create({
@@ -21,9 +22,38 @@ export class ProductService {
             where: { id },
         });
 
-        if(!product) {
+        if (!product) {
             throw new NotFoundException(`Product with ID ${id} not found`)
         }
         return product;
+    }
+
+    async update(id: number, updateProductDto: UpdateProductDto) {
+        const product = await this.prisma.product.findUnique({
+            where: { id },
+        });
+
+        if (!product) {
+            throw new NotFoundException(`Product with ID ${id} not found`)
+        }
+
+        return this.prisma.product.update({
+            where: { id },
+            data: updateProductDto,
+        });
+    }
+
+    async remove(id: number) {
+        const product = await this.prisma.product.findUnique({
+            where: { id },
+        });
+
+        if (!product) {
+            throw new NotFoundException(`Product with ID ${id} not found`)
+        }
+
+        return this.prisma.product.delete({
+            where: { id },
+        });
     }
 }
