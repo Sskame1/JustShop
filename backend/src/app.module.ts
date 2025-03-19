18a7f49ccd304b88731324492ee538173bba3a13
Anatoliy <119749324+Sskame1@users.noreply.github.com>
2025-03-19 20:42:04 +0900

    nest g res product

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, ProductModule],
   controllers: [],
   providers: [PrismaService],
 })
