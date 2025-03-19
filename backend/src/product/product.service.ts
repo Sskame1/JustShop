a3e9d2a5d753f777d742d61cc605f87c61a00dc1
Anatoliy <119749324+Sskame1@users.noreply.github.com>
2025-03-19 21:02:34 +0900

    changed product service(added func create and findAll product)

--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateProductDto } from './create-product.dto';
 
 @Injectable()
 export class ProductService {
     constructor(private prisma: PrismaService) {}
+
+    async create(createProductDto: CreateProductDto) {
+        return this.prisma.product.create({
+            data: createProductDto,
+        });
+    }
+
+    async findAll() {
+        return this.prisma.product.findMany();
+    }
 
     
 }
