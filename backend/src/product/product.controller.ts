6a6de5c74d706bd160820926c6874e9695318e82
Anatoliy <119749324+Sskame1@users.noreply.github.com>
2025-03-19 21:16:48 +0900

    Set up JWT for Products and added in controller create

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { Roles } from 'src/auth/roles.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { CreateProductDto } from './create-product.dto';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  @Roles('ADMIN', 'SELLER')
+  @UseGuards(RolesGuard)
+  async create(@Body() createProductDto: CreateProductDto) {
+    return this.productService.create(createProductDto);
+  }
 }
--- backend/src/product/product.module.ts
+++ backend/src/product/product.module.ts
@@ -2,8 +2,20 @@ import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
+  imports: [
+      JwtModule.registerAsync({
+        imports: [ConfigModule],
+        useFactory: async (configService: ConfigService) => ({
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: { expiresIn: '1h' },
+        }),
+        inject: [ConfigService],
+      }),
+    ],
   controllers: [ProductController],
   providers: [ProductService, PrismaService],
 })
