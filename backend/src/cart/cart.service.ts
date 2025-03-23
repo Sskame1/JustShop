7a33e521fbfe2fcc05cc27e910f2aeff4cdd353f
Anatoliy <119749324+Sskame1@users.noreply.github.com>
2025-03-23 16:56:48 +0900

    added in module cart(in providers PrismaService)

--- backend/src/cart/cart.service.ts
+++ backend/src/cart/cart.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class CartService {}
+export class CartService {
+    constructor(private prisma: PrismaService) {}
+
+    async getCart(userId: number) {
+        return this.prisma.cart.findUnique({
+            where: { userId },
+            include: { items: { include: { product: true } } },
+        });
+    }
+}
