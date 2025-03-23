a2919999bf565d788f470b860ec9086c82e623e6
Anatoliy <119749324+Sskame1@users.noreply.github.com>
2025-03-23 16:36:46 +0900

    nest g res cart

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ProductModule],
+  imports: [AuthModule, UserModule, ProductModule, CartModule],
   controllers: [],
   providers: [PrismaService],
 })
