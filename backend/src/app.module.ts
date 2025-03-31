767d27b7bffb64e14b0a2465253a49f8fee33f46
Sskame1 <tolikstrgonov111@gmail.com>
2025-03-31 13:31:47 +0900

    nest g res order

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
 import { CartModule } from './cart/cart.module';
 import { PassportModule } from '@nestjs/passport';
+import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
@@ -12,7 +13,8 @@ import { PassportModule } from '@nestjs/passport';
     PassportModule.register({ session: false }),
     UserModule,
     ProductModule,
-    CartModule
+    CartModule,
+    OrderModule
   ],
   controllers: [],
   providers: [PrismaService],
